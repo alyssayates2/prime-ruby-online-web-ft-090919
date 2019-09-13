c80ea14b451907a16a8e4c3489a5d53d8ec181ab
Alyssa Yates <bosslys@icloud.com>
2019-09-13 21:58:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,10 @@
-require 'prime'
-Prime.prime?(2)
-Prime.prime?(4)
+def prime?(n)
+  if n <= 1
+    return false
+  elsif n <= 3
+    return true
+  else (2..n/2).none? do |x|
+    n % x == 0
+  end
+  end
+end
